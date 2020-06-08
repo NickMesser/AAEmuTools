10b7920f8dc6e2c84f86e1a5c4ee0f2c901122b8
Hucaru <7339035+Hucaru@users.noreply.github.com>
2020-06-08 20:50:53 +0100

    Updated script

--- ConvertDumpToJson/app.py
+++ ConvertDumpToJson/app.py
@@ -1,75 +1,85 @@
 import json
+import sys
+import os
+import sqlite3
+import argparse
+import math
 
-attachTypes = {
-    '$driver':1,
-    '$passenger0':2,
-    '$passenger1':3,
-    '$passenger2':4,
-    '$passenger3':5,
-    '$passenger4':6,
-    '$passenger5':7,
-    '$passenger6':8,
-    '$cannon0':9,
-    '$cannon1':10,
-    '$cannon2':11,
-    '$cannon3':12,
-    '$cannon4':13,
-    '$cannon5':14,
-    '$cannot6':15,
-    '$cannon7':16,
-    '$cannon8':17,
-    '$hook':18,
-    '$mast0':19,
-    '$mast1':20,
-    '$sail0':21,
-    '$sail1':22,
-    '$anchor':23,
-    '$ladder_left':24,
-    '$ladder_right':25,
-    '$plank_left':26,
-    '$plank_right':27,
-    '$lamp_front':28,
-    '$lamp_rear':29,
-    '$trailed0':30,
-    '$trailed1':31,
-    '$trailed2':32,
-    '$trailed3':33,
-    '$trailed4':34,
-    '$helms':35,
-    '$heal_point_0':36,
-    '$heal_point_1':37,
-    '$heal_point_2':38,
-    '$heal_point_3':39,
-    '$heal_point_4':40,
-    '$heal_point_5':41,
-    '$heal_point_6':42,
-    '$heal_point_7':43,
-    '$heal_point_8':44,
-    '$heal_point_8':45,
-    '$cannon10':47,
-    '$cannon11':48,
-    '$cannon12':49,
-    '$ladder_rear_right':58,
-    '$ladder_rear_left':59
- }
+slaveNameLookup = {
+    'speedboat_a':128,
+    'soloship_a': 1288,
+    'boardship_body': 1249,
+    'smallboat_body': 129,
+    'galleon_body': 393,
+    'fishboat': 1360,
+    'boxship': 1205,
+    'cruise_a': 1046
+}
 
+def calc_angle(v, axis):
+    angle = 0
 
-data = {}
-data['ModelID'] = 128 #Must change to match current model
-data['AttachPointId'] = []
+    if axis is 'x':
+        angle = v[0] / math.sqrt(v[0]**2 + v[1]**2 + v[2]**2)
+    elif axis is 'y':
+        angle = v[1] / math.sqrt(v[0]**2 + v[1]**2 + v[2]**2)
+    elif axis is 'z':
+        angle = v[2] / math.sqrt(v[0]**2 + v[1]**2 + v[2]**2)
 
-with open('speedboat.dump','r') as reader: #Must change this to match filename you want to convert
-    lines = reader.readlines()
+    return angle
 
-for idx,line in enumerate(lines):
-    for key in attachTypes:
-        if key in line:
-            coordSplit = lines[idx+2].split('trans')
-            coords = coordSplit[1][1:-2].split(' ')
-            x = float(coords[0])
-            y = float(coords[1])
-            z = float(coords[2])
-            data['AttachPointId'].append({attachTypes[key]:{"X":x,"Y":y,"Z":z}})
+if __name__=="__main__":
+    parser = argparse.ArgumentParser(description='cfg model to attach point convertor')
+    parser.add_argument('--db', type=str, help='compact db file path')
+    parser.add_argument("--cfg", type=str, nargs='+', help='cfg dumps to parse')
+    args = parser.parse_args()
 
-with open('output.json', 'w') as outfile:
-    json.dump(data, outfile, indent=4)
+    attachTypes = {}
+
+    conn = sqlite3.connect(args.db)
+    c = conn.cursor()
+
+    for row in c.execute('SELECT id, prefab FROM model_attach_point_strings'):
+        attachTypes[row[1]] = row[0]
+
+    output = []
+
+    for i, filename in enumerate(args.cfg):
+        print("Parsing file {}/{} {}".format(i+1, len(args.cfg), filename))
+
+        modelName = os.path.basename(filename).split('.')[0]
+
+        if modelName not in slaveNameLookup:
+            print("Model name {} not found in slave lookup".format(modelName))
+            continue
+        
+        data = {}
+        data['ModelId'] = slaveNameLookup[modelName]
+        data['AttachPoints'] = {}
+
+        with open(filename,'r') as reader:
+            lines = reader.readlines()
+        
+        for idx,line in enumerate(lines):
+            for key in attachTypes:
+                if key in line:
+                    coordSplit = lines[idx+2].split('trans')
+                    coords = coordSplit[1][1:-2].split(' ')
+                    x = float(coords[0])
+                    y = float(coords[1])
+                    z = float(coords[2])
+
+                    # xRotation = [*map(lambda x: float(x), lines[idx+2].split('axisX(')[1].split(')')[0].split(' '))]
+                    # yRotation = [*map(lambda x: float(x), lines[idx+2].split('axisY(')[1].split(')')[0].split(' '))]
+                    # zRotation = [*map(lambda x: float(x), lines[idx+2].split('axisZ(')[1].split(')')[0].split(' '))]
+                    
+                    data['AttachPoints'][attachTypes[key]] = {
+                        "X":x,"Y":y,"Z":z
+                        # "RotationX": calc_angle(xRotation,'x') ,"RotationY": calc_angle(yRotation, 'y'),"RotationZ": calc_angle(zRotation, 'z')
+                    }
+                    
+        output.append(data)
+
+    with open('output.json', 'w') as outfile:
+        json.dump(output, outfile, indent=4)
+
