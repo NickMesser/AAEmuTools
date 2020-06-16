6119b119d63c8670ad2cb1558b729bbf980cc927
Hucaru <7339035+Hucaru@users.noreply.github.com>
2020-06-16 18:53:39 +0100

    Initial rotation calculation added

--- ConvertDumpToJson/app.py
+++ ConvertDumpToJson/app.py
@@ -4,6 +4,7 @@ import os
 import sqlite3
 import argparse
 import math
+import re
 
 slaveNameLookup = {
     'speedboat_a':128,
@@ -15,6 +16,60 @@ slaveNameLookup = {
     'boxship': 1205,
     'cruise_a': 1046
 }
+
+regex_pattern = re.compile("localTM: axisX\((.+)\s(.+)\s(.+)\) axisY\((.+)\s(.+)\s(.+)\) axisZ\((.+)\s(.+)\s(.+)\) trans\((.+)\s(.+)\s(.+)\)")
+
+# Axis of rotation
+# x-axis
+# 1 0 0
+# 0 v v
+# 0 v v
+
+# y-axis
+# v 0 v
+# 0 1 0
+# v 0 v
+
+# z-axis
+# v v 0
+# v v 0
+# 0 0 1
+
+matrix_lookup = {
+    "pi/2": {"matrix": [[0.0, -1.0, 0.0], 
+                        [1.0, 0.0, 0.0], 
+                        [0.0, 0.0, 1.0]], 
+            "rotation": (0, 0, -64)},
+    "pi": {"matrix": [[-1.0, 0.0, 0.0], 
+                      [0.0, -1.0, 0.0], 
+                      [0.0, 0.0, 1.0]], 
+            "rotation": (0, 0, -127)},
+    "3pi/2": {"matrix": [[0.0, 1.0, 0.0], 
+                        [-1.0, 0.0, 0.0], 
+                        [0.0, 0.0, 1.0]], 
+            "rotation": (0, 0, 64)}
+}
+
+def calc_rotation(axis_x, axis_y, axis_z):
+    # Not sure how to deal with values of magnitude ~e-7 n.
+    # Since they are << 1 treat as zero?
+    for i, v in enumerate(axis_x):
+        axis_x[i] = round(v)
+
+    for i, v in enumerate(axis_y):
+        axis_y[i] = round(v)
+
+    for i, v in enumerate(axis_z):
+        axis_z[i] = round(v)
+
+    # Construct matrix
+    matrix = [axis_x, axis_y, axis_z]
+
+    for k, v in matrix_lookup.items():
+        if v["matrix"] == matrix:
+            return v["rotation"]
+
+    return (0, 0, 0)
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description='cfg model to attach point convertor')
@@ -51,14 +106,27 @@ if __name__=="__main__":
         for idx,line in enumerate(lines):
             for key in attachTypes:
                 if key in line:
-                    coordSplit = lines[idx+2].split('trans')
-                    coords = coordSplit[1][1:-2].split(' ')
-                    x = float(coords[0])
-                    y = float(coords[1])
-                    z = float(coords[2])
+                    search_result = re.search(regex_pattern, lines[idx+2])
+
+                    if search_result is None:
+                        print("Regex did not match for {}", filename)
+                        continue
+
+                    axis_x = [float(search_result.group(1)), float(search_result.group(2)), float(search_result.group(3))]
+                    axis_y = [float(search_result.group(4)), float(search_result.group(5)), float(search_result.group(6))]
+                    axis_z = [float(search_result.group(7)), float(search_result.group(8)), float(search_result.group(9))]
+
+                    rotation = calc_rotation(axis_x, axis_y, axis_z)
+
+                    trans_x = float(search_result.group(10))
+                    trans_y = float(search_result.group(11))
+                    trans_z = float(search_result.group(12))
 
                     data['AttachPoints'][attachTypes[key]] = {
-                        "X":x,"Y":y,"Z":z
+                        "X":trans_x,"Y":trans_y,"Z":trans_z,
+                        "RotationX": rotation[0],
+                        "RotationY": rotation[1],
+                        "RotationZ": rotation[2],
                     }
                     
         output.append(data)
